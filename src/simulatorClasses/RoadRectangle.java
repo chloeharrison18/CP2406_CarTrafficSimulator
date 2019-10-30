6d1faebc661f4f68e619c6c86d6329acdc4ffbc2
Chloe <chloe.harrison@my.jcu.edu.au>
2019-10-30 22:00:27 +1000

    Add RoadRectangle class and tests

--- src/simulatorClasses/RoadRectangle.java
+++ src/simulatorClasses/RoadRectangle.java
@@ -3,14 +3,14 @@ package simulatorClasses;
 import javax.swing.*;
 import java.awt.*;
 
-public class CarRectangle extends JPanel {
+public class RoadRectangle extends JPanel {
     @Override
     public void paintComponent(Graphics gr) {
         super.paintComponent(gr);
         int x = 200;
         int y = 200;
-        final int WIDTH = 100, HEIGHT = 80;
-        gr.setColor(Color.RED);
+        final int WIDTH = 100, HEIGHT = 100;
+        gr.setColor(Color.DARK_GRAY);
         gr.fillRect(x, y, WIDTH, HEIGHT);
     }
 }
---
+++ src/simulatorClasses/RoadRectangleTest.java
@@ -0,0 +1,15 @@
+package simulatorClasses;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoadRectangleTest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new RoadRectangle());
+        frame.setSize(430, 180);
+        frame.setVisible(true);
+    }
+}
