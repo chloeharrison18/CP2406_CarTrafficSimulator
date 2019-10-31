ed455b158e78b795208901d934cb44d04425bc1b
Chloe <chloe.harrison@my.jcu.edu.au>
2019-10-31 11:30:54 +1000

    Update TrafficLightRectangle class

--- src/simulatorClasses/TrafficLightRectangle.java
+++ src/simulatorClasses/TrafficLightRectangle.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TrafficLightRectangle extends JPanel {
+    private final int WIDTH = 30, HEIGHT = 60;
+    private int x;
+    private int y;
+
+    public TrafficLightRectangle(int xCoordinate, int yCoordinate) {
+        x = xCoordinate;
+        y = yCoordinate;
+    }
+
     @Override
     public void paintComponent(Graphics gr) {
         super.paintComponent(gr);
-        int x = 200;
-        int y = 100;
-        final int WIDTH = 30, HEIGHT = 60;
         gr.setColor(Color.GRAY);
         gr.fillRect(x, y, WIDTH, HEIGHT);
     }
--- src/simulatorClasses/TrafficLightRectangleTest.java
+++ src/simulatorClasses/TrafficLightRectangleTest.java
@@ -8,7 +8,7 @@ class TrafficLightRectangleTest {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new TrafficLightRectangle());
+        frame.add(new TrafficLightRectangle(100, 100));
         frame.setSize(430, 180);
         frame.setVisible(true);
     }
